e1782a45ab2c426a2c88b42de9482f46436d30a9
Agata <amomot@bio-prodict.nl>
2022-12-01 12:59:48 +0100

    Enter skeletone is probably finished

--- workshop/src/cp2022/solution/WorkshopImplemented.java
+++ workshop/src/cp2022/solution/WorkshopImplemented.java
@@ -30,9 +30,9 @@ public class WorkshopImplemented implements Workshop {
 
     /* Workplace data */
     // Indicates whether the user can seat at the given workplace
-    private final HashMap<WorkplaceId, Boolean> isAvailableToSeatAt = new HashMap<>();
+    private final ConcurrentHashMap<WorkplaceId, Boolean> isAvailableToSeatAt = new ConcurrentHashMap<>();
     // Idicates whether the user can start using (call use()) at the given workplace
-    private final HashMap<WorkplaceId, Boolean> isAvailableToUse = new HashMap<>();
+    private final ConcurrentHashMap<WorkplaceId, Boolean> isAvailableToUse = new ConcurrentHashMap<>();
 
     /* Synchronization of the counter of possible number of entries to satisfy 2*N rule */
     private Semaphore mutexEntryCounter = new Semaphore(1);
@@ -40,16 +40,21 @@ public class WorkshopImplemented implements Workshop {
     private Semaphore waitForEntry = new Semaphore(0, true); // FIFO semaphore
 
     /* Synchronization of the access to the workplace data */
-    private Semaphore mutexWorkplaceData = new Semaphore(1);
+    // private Semaphore mutexWorkplaceData = new Semaphore(1);
+    private final ConcurrentHashMap<WorkplaceId, Semaphore> mutexWorkplaceData = new ConcurrentHashMap<>();
 
-    /* Synchronization of the access to the seat at the given workplace */
+    /* Synchronization of the access to the seat at the given workplace
+    *  Mutex protects also workplace data isAvailableToSeat
+    * */
     // private Semaphore mutexWaitForSeat = new Semaphore(1);
     // private long howManyWaitForSeat = 0;
     private final ConcurrentHashMap<WorkplaceId, Semaphore> mutexWaitForASeat = new ConcurrentHashMap<>();
     private final ConcurrentHashMap<WorkplaceId, Long> howManyWaitForASeat = new ConcurrentHashMap<>();
     private final ConcurrentHashMap<WorkplaceId, Semaphore> waitForSeat = new ConcurrentHashMap<>();
 
-    /* Synchronization of the permission to use (call use()) the given workplace */
+    /* Synchronization of the permission to use (call use()) the given workplace
+    *  Mutex protects also workplace data isAvailableToSeat
+    * */
     // private Semaphore mutexWaitToUse = new Semaphore(1);
     // private long howManyWaitToUse = 0;
     private final ConcurrentHashMap<WorkplaceId, Semaphore> mutexWaitToUse = new ConcurrentHashMap<>();
@@ -82,6 +87,8 @@ public class WorkshopImplemented implements Workshop {
 
             mutexWaitToUse.putIfAbsent(placeId, new Semaphore(1));
             mutexWaitForASeat.putIfAbsent(placeId, new Semaphore(1));
+
+
         }
     }
 
@@ -123,7 +130,7 @@ public class WorkshopImplemented implements Workshop {
         try {
             mutexEntryCounter.acquire();
 
-            entryCounter.putIfAbsent(currentThreadId, 2*maxEntries);
+            entryCounter.putIfAbsent(currentThreadId, 2*maxEntries); // TODO move before mutex?
             long minimumPossibleEntries = Collections.min(entryCounter.values());
             if (minimumPossibleEntries == 0) {
                 howManyWaitForEntry++;
@@ -144,12 +151,30 @@ public class WorkshopImplemented implements Workshop {
         // therefore entryCounter is not updated upon the given thread entry completion,
         // confirmed at the beginning of decorative use() implementation.
         try {
+            Semaphore mutexMyWorkplace = mutexWaitForASeat.get(wid);
 
+            // We need concurrent map to safely access particular workshop data,
+            // but mutex is used to synchronize related operations
+            mutexMyWorkplace.acquire();
+            if (!isAvailableToSeatAt.get(wid)) {
+                howManyWaitForASeat.compute(wid, (key, val) -> val++); // TODO does it work?
+                Semaphore mySeatSemaphore = waitForSeat.get(wid);
+                mutexMyWorkplace.release();
+
+                mySeatSemaphore.acquire();
+
+                howManyWaitForASeat.compute(wid, (key, val) -> val--);
+            }
+
+            isAvailableToSeatAt.replace(wid, false); // The current user is entering and later will call use()
+            mutexMyWorkplace.release();
+
+            return availableWorkplaces.get(wid);
         } catch (InterruptedException e) {
             throw new RuntimeException("panic: unexpected thread interruption");
         }
 
-        return null;
+        // return null;
     }
 
     @Override
