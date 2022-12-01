f4e14f873175dd0e032a827dcaee904140aeb707
Agata <amomot@bio-prodict.nl>
2022-12-01 11:14:45 +0100

    Data is probably declared and initialized

--- workshop/src/cp2022/solution/WorkshopImplemented.java
+++ workshop/src/cp2022/solution/WorkshopImplemented.java
@@ -6,7 +6,6 @@ import cp2022.base.Workshop;
 
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Semaphore;
 
@@ -24,6 +23,7 @@ public class WorkshopImplemented implements Workshop {
     // Actual workplace a given thread is seated at: <ThreadId, WorkplaceId>
     private final ConcurrentHashMap<Long, WorkplaceId> previousWorkplace = new ConcurrentHashMap<>();
 
+    /* Workplace data */
     // Indicates whether the user can seat at the given workplace
     private final HashMap<WorkplaceId, Boolean> isAvailableToSeatAt = new HashMap<>();
     // Idicates whether the user can start using (call use()) at the given workplace
@@ -34,7 +34,20 @@ public class WorkshopImplemented implements Workshop {
     private long howManyWaitForEntry = 0;
     private Semaphore waitForEntry = new Semaphore(0, true); // FIFO semaphore
 
-    /* Synchronization of the access to workplace data */
+    /* Synchronization of the access to the workplace data */
+    private Semaphore mutexWorkplaceData = new Semaphore(1);
+
+    /* Synchronization of the access to the seat at the given workplace */
+    private Semaphore mutexWaitForSeat = new Semaphore(1);
+    private long howManyWaitForSeat = 0;
+    private final ConcurrentHashMap<WorkplaceId, Semaphore> waitForSeat = new ConcurrentHashMap<>();
+
+    /* Synchronization of the permission to use (call use()) the given workplace */
+    private Semaphore mutexWaitToUse = new Semaphore(1);
+    private long howManyWaitToUse = 0;
+    private final ConcurrentHashMap<WorkplaceId, Semaphore> waitToUse = new ConcurrentHashMap<>();
+
+
 
     private void createAvailableWorkplaceHashmap(Collection<Workplace> workplaces) {
         for (Workplace place: workplaces) {
@@ -44,13 +57,29 @@ public class WorkshopImplemented implements Workshop {
 
     private void initializeWorkplaceData(Collection<Workplace> workplaces) {
         for (Workplace place: workplaces) {
-            
+            isAvailableToSeatAt.putIfAbsent(place.getId(), true);
+            isAvailableToUse.putIfAbsent(place.getId(), true);
         }
     }
 
-    public WorkshopImplemented(Collection<Workplace> workplaces) {
+    private void initializationOfSemaphoreMaps(Collection<Workplace> workplaces) {
+        for (Workplace place: workplaces) {
+            waitForSeat.putIfAbsent(place.getId(), new Semaphore(0, true));
+            waitToUse.putIfAbsent(place.getId(), new Semaphore(0, true));
+        }
+    }
+
+    private void constructorDataInitialization(Collection<Workplace> workplaces) {
         createAvailableWorkplaceHashmap(workplaces);
+        initializeWorkplaceData(workplaces);
+        initializationOfSemaphoreMaps(workplaces);
+    }
+
+    public WorkshopImplemented(Collection<Workplace> workplaces) {
+        constructorDataInitialization(workplaces);
         this.maxEntries = workplaces.size();
+
+        // FIXME remove
         printout();
     }
 
